1a3f6d21fa3ffff9ebf9cf1edd19602fa5c0a721
daliahatari <dan20160761@std.psut.edu.jo>
2022-04-08 22:26:12 +0300

    Final update

--- WeCater/app/src/main/java/com/example/wecater/MainActivity.kt
+++ WeCater/app/src/main/java/com/example/wecater/MainActivity.kt
@@ -10,21 +10,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firstFragment = Appetizers()
+        val secondFragment = PlaceOrder()
+        val thirdFragment = ContactFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.FLFragment, firstFragment)
+            commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.my_first_menu, menu)
         return super.onCreateOptionsMenu(menu)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.order -> Toast.makeText(this, "Place Order Option Selected", Toast.LENGTH_SHORT).show()
-            R.id.contact -> Toast.makeText(this, "Contact Us Option Selected", Toast.LENGTH_SHORT).show()
-            R.id.info -> Toast.makeText(this, "Info Option Selected", Toast.LENGTH_SHORT).show()
-        }
+             R.id.order -> supportFragmentManager.beginTransaction().apply {
+                 replace(R.id.FLFragment, PlaceOrder())
+                 commit()
+             }
 
+            R.id.contact -> supportFragmentManager.beginTransaction().apply {
+                replace(R.id.FLFragment, ContactFragment())
+                commit()
+            }
+
+            R.id.info -> supportFragmentManager.beginTransaction().apply {
+                replace(R.id.FLFragment, Appetizers())
+                commit()
+            }
+        }
         return true
     }
 }
+
+
+
